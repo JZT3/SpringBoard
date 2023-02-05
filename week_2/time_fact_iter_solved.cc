f138f7c5cb551011d77fecd6b3f00cd136e8bb6e
Jervani Z Thompson <jzthompson20@gmail.com>
2023-02-05 13:23:07 -0600

    Create time_fact_iter_solved.cc
    Iterative implementation of factorial.

---
+++ week_2/time_fact_iter_solved.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+
+using namespace std;
+
+int64_t fact(int & N){
+  int64_t next_product = 1;
+  
+  for(int i=1; i<=N;i++){
+    next_product = next_product * (i);
+  }
+
+  return next_product;
+}
+
+int main(){
+  for(int N=0;N<=20;N++){
+    auto start = chrono::high_resolution_clock::now();
+
+    int64_t f = fact(N);
+
+    auto end = chrono::high_resolution_clock::now();
+    auto elapsed = chrono::duration<double>(end-start).count();
+
+    cout << "N= "<<setw(2) << N << " "<<"N!= "<<setw(18)<<f<<" "<<"elapsed= "<< scientific<<setprecision(6)<<elapsed<<" [sec]"<<endl;
+  }
+    return 0;
+  }
